469b06bf3b4cabf01c1b8b3c32e040e7cc83c1c3
tianitiara <42743959+tianitiara@users.noreply.github.com>
2018-09-28 12:06:38 +0700

    homepage deluxe tea

---
+++ application/views/homepagedeluxetea.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<h3><title> DELUXE TEA</title><h3>	
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url()?>asset/css/desain.css>
+	<link rel="stylesheet" type="text/css" href=<?php echo base_url()?>asset/css/bootstrap.css>
+</head>
+
+<body>
+<div id="header">
+	<h3><a href="" class="title" style="color: #F0FFFF; font-family: arial,verdana,sans-serif; position: relative;">Deluxe Tea Real-time Stock Management</a><h3>
+	<div class=logout></div>
+</div>
+
+<div class="list-blog single-post d-sm-flex wow fadeInUpBig">
+	<div id="content">
+		<ul>
+		<li><a href="#">Lihat Data Stock</a></li>
+		<li><a href="#">Input Stock</a></li>
+		<li><a href="#">Update Stock</a></li>
+		<li><a href="#">Input Data Partner</a></li>
+		<li><a href="#">Edit Partner</a></li>
+		<li><a href="#">Delete Partner</a></li>
+		<li><a href="#">Input Data Admin</a></li>
+		<li><a href="#">Delete Admin</a></li>
+		</ul>
+	</div>
+	<div class="post-thumb">
+       	<img class="gambar" src=<?php echo base_url()?>asset/image/ponyyyy.jpg />
+	</div>
+</div>
+</body>
+</html>
