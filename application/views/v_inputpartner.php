581a11d5d1d916d8ff1b5662591c4114a632b768
hasbiabdulmajid <42906732+hasbiabdulmajid@users.noreply.github.com>
2018-12-15 14:04:52 +0700

    update link CSS
    link css pake php

--- application/views/v_inputpartner.php
+++ application/views/v_inputpartner.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <link rel="stylesheet" href="Untitled.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/Untitled.css">
  <!--  <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> -->
 </head>
 
